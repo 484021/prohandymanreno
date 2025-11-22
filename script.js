504932f62f57d0e940cd14a933e576092141dbbf
484021 <bhoopalsanthosh@gmail.com>
2025-11-22 10:14:23 -0500

    feat: Homepage layout completed

--- script.js
+++ script.js
@@ -64,21 +64,3 @@ function togglePlayButton() {
 
 // Call the function to activate the event listeners
 togglePlayButton();
-
-function toggleVideoPlayback() {
-  // Select the video element
-  const video = document.querySelector("#sbs-2329 video");
-
-  // Add a click event listener to the video
-  video.addEventListener("click", () => {
-    // Check if the video is paused
-    if (video.paused) {
-      video.play(); // Play the video if it is paused
-    } else {
-      video.pause(); // Pause the video if it is playing
-    }
-  });
-}
-
-// Call the function to activate the event listener
-toggleVideoPlayback();
